2986ef16769deb33e07532b2d851bbf4b5f3c044
faw615 <faw615@mail.usask.ca>
2017-09-29 10:42:09 -0600

    Implemented starwar

--- starwar.go
+++ starwar.go
@@ -82,10 +82,10 @@ func readString(s string) string {
 
 type Graph [][]bool
 
+var allPath [][]string
 var visit []int
 var cost []int
-var path []string
-
+var count []int
 var s map[string]int
 
 func readVertices() Graph {
@@ -96,7 +96,8 @@ func readVertices() Graph {
 		os.Exit(0)
 	}
 	g := make(Graph, V, V)
-	//visit := make([]int, V, V)
+	count = make([]int, V, V)
+	visit = make([]int, V, V)
 	for v := 0; v < V; v++ {
 		c := 0
 		name := readString("star-system")
@@ -126,11 +127,84 @@ func readEdge(g Graph, E int) {
 	}
 }
 
+func mapkey(m map[string]int, value int) (key string, ok bool) {
+	for k, v := range m {
+		if v == value {
+			key = k
+			ok = true
+			return
+		}
+	}
+	return
+}
+
+func DFS(g Graph, start string, end string) {
+	stack := NewStack()
+	var dive func(g Graph, start string, end string)
+	dive = func(g Graph, start string, end string) {
+		stack.Push(start)
+		visit[s[start]] = 1
+		for {
+			if start == end {
+				var path []string
+				for i := 0; i < stack.Size(); i++ {
+					path = append(path, stack.Get(i).(string))
+				}
+				allPath = append(allPath, path)
+				stack.Pop()
+				visit[s[start]] = 0
+				break
+			}
+			for i := range g {
+				if g[s[start]][i] {
+					if 0 == visit[i] {
+						star, _ := mapkey(s, i)
+						dive(g, star, end)
+					}
+				}
+			}
+			stack.Pop()
+			visit[s[start]] = 0
+			break
+		}
+	}
+	dive(g, start, end)
+}
+
 func main() {
 	//	dat, _ := ioutil.ReadFile("./test2.in")
 	//	fmt.Println(string(dat))
 	g := readVertices()
 	E := readInt("E")
 	readEdge(g, E)
-	fmt.Println(g, E, cost)
+	DFS(g, "Scarif", "Yavin")
+
+	for k, _ := range allPath {
+		for _, v := range allPath[k] {
+			if v != "Scarif" && v != "Yavin" {
+				count[s[v]]++
+			}
+		}
+	}
+	var keypoint []string
+	for k, v := range count {
+		if v == len(allPath) {
+			star, _ := mapkey(s, k)
+			keypoint = append(keypoint, star)
+		}
+	}
+	if nil == keypoint {
+		fmt.Println("Leia escapes with the plans!")
+	} else {
+
+		temp := 0
+		ctemp := 1<<31 - 1
+		for k, v := range keypoint {
+			if cost[s[v]] <= ctemp {
+				ctemp = cost[s[v]]
+				temp = k
+			}
+		}
+		fmt.Printf("Darth blockades %s (%d).", keypoint[temp], cost[s[keypoint[temp]]])
+	}
 }
